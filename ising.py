e2298324da18b8324e67c0643b123b2cbc9d758c
cacciutolab <jwymbs23>
2017-09-19 16:42:01 -0400

    changed np array to list

--- ising.py
+++ ising.py
@@ -9,11 +9,13 @@ import re
 
 
 
-n=150
 
-ising_array = np.ones((n,n))
+#def initialize():
+#global ising_array, J, H, beta, n
+n=200
+ising_array = [[-1 if (random.random() > 0.5) else 1  for i in range(n)] for j in range(n)]
 #turn into list ^^^
-J = 10
+J = 1
 H = 0.0
 beta = 1
 
@@ -37,7 +39,7 @@ def calc_total_energy(J):
     for i in range(n):
         for j in range(n):
             #H = -J \SUM sigma_i sigma_j {+1,-1}
-            energy += -J*ising_array[i,(j+1)%n] - J*ising_array[(i+1)%n,j] + H*ising_array[i,j]
+            energy += -J*ising_array[i][(j+1)%n] - J*ising_array[(i+1)%n][j] + H*ising_array[i][j]
     return energy
 
 
@@ -46,10 +48,11 @@ def calc_del_e(spin, J):
     #print(get_neighbors(spin))
     i = spin[0]
     j = spin[1]
-    neigh = ising_array[[(i+1)%n,i,(i-1)%n,i],[j,(j+1)%n,j,(j-1)%n]]
-    sigma_spin = ising_array[i,j]
-    old_e = -J*np.sum(sigma_spin*neigh) + H*sigma_spin
-    new_e = -J*np.sum(-sigma_spin*neigh) - H*sigma_spin
+    neigh = [ising_array[(i+1)%n][j], ising_array[i][(j+1)%n], ising_array[(i-1)%n][j], ising_array[i][(j-1)%n]]
+    #when ising array is a numpy array neigh = ising_array[[(i+1)%n,i,(i-1)%n,i],[j,(j+1)%n,j,(j-1)%n]]
+    sigma_spin = ising_array[i][j]
+    old_e = -J*sum([sigma_spin*i for i in neigh]) + H*sigma_spin
+    new_e = -J*sum([-sigma_spin*i for i in neigh]) - H*sigma_spin
     return (new_e - old_e)
 
 spin = [0,0]
@@ -60,20 +63,19 @@ tot_en = calc_total_energy(J)
 
 random.seed(1002)
 sweeps = 500
-randomize()
+#initialize()
 
 for s in range(sweeps):
-
-    t0 = time.time()
+#    t0 = time.time()
     for sw in range(n*n):
         spin = [random.randint(0,n-1),random.randint(0,n-1)]
         del_e = calc_del_e(spin, J)
         if(exp(-beta*del_e) > random.random()):
-            ising_array[spin[0],spin[1]] *= -1
+            ising_array[spin[0]][spin[1]] *= -1
             tot_en += del_e
-    plt.cla()
-    plt.imshow(ising_array)
-    plt.pause(0.0001)
-    t1 = time.time()
-    print(t1 - t0)
+#    plt.cla()
+#    plt.imshow(ising_array)
+#    plt.pause(0.0001)
+#    t1 = time.time()
+#    print(t1 - t0)
 
